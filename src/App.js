da9bbfaefab803e7154fc08fab7e3120d5c93631
ranaintizar <intizaralirana2@gmail.com>
2023-12-15 12:08:01 +0500

    Minor Fixes

--- src/App.js
+++ src/App.js
@@ -1,13 +1,11 @@
 import Footer from './components/footer'
 import Header from './components/header'
 import Main from './components/main'
-import Nav from './components/nav'
 
 function App() {
   return (
     <>
       <Header />
-      <Nav />
       <Main />
       <Footer />
     </>
--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -1,9 +1,12 @@
 import React from 'react'
 
+import Nav from '../nav'
+
 const Header = () => {
   return (
     <header>
       <img />
+      <Nav />
     </header>
   )
 }
